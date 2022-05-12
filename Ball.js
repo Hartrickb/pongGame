e35ae4454ce079172e064c29dc2014a517ae4a4c
Hartrickb <hartrickb@users.noreply.github.com>
2022-05-12 15:11:39 -0400

    ball detects sides of screen

--- Ball.js
+++ Ball.js
@@ -1,4 +1,5 @@
 const INITIAL_VELOCITY = 0.025;
+const VELOCITY_INCREASE = 0.00001;
 
 export default class Ball {
   constructor(ballElem) {
@@ -22,13 +23,17 @@ export default class Ball {
     this.ballElem.style.setProperty('--y', value);
   }
 
+  rect() {
+    return this.ballElem.getBoundingClientRect();
+  }
+
   reset() {
     this.x = 50;
     this.y = 50;
     this.direction = { x: 0 };
     while (
       Math.abs(this.direction.x <= 0.2) ||
-      Math.abs(this.direction >= 0.9)
+      Math.abs(this.direction.x >= 0.9)
     ) {
       const heading = randomNumberBetween(0, 2 * Math.PI);
       this.direction = { x: Math.cos(heading), y: Math.sin(heading) };
@@ -39,6 +44,16 @@ export default class Ball {
   update(delta) {
     this.x += this.direction.x * this.velocity * delta;
     this.y += this.direction.y * this.velocity * delta;
+    this.velocity += VELOCITY_INCREASE * delta;
+    const rect = this.rect();
+
+    if (rect.bottom >= window.innerHeight || rect.top <= 0) {
+      this.direction.y *= -1;
+    }
+
+    if (rect.right >= window.innerWidth || rect.left <= 0) {
+      this.direction.x *= -1;
+    }
   }
 }
 
